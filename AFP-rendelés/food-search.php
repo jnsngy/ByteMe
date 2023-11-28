a9e9aa4cef923acb2e38d855e42befa52b151508
ArchibaldLance <149420538+ArchibaldLance@users.noreply.github.com>
2023-11-28 18:16:36 +0100

    menu section

---
+++ AFP-rendelés/food-search.php
@@ -0,0 +1,33 @@
+<?php include('partials-front/menu.php');?>
+
+    <!-- keresés kezdete -->
+    <section class="food-search text-center">
+        <div class="container">
+
+        <?php 
+
+        //a keresési kulcsszó 
+
+        $search = $_POST['search'];
+        
+        ?>
+            
+            <h2>Ételek a keresés alapján: <a href="#" class="text-white">"<?php echo $search;?>"</a></h2>
+
+        </div>
+    </section>
+    <!-- keresés vége -->
+
+    <!-- menü kezdete -->
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Étel menü</h2>
+
+            <?php 
+                //sql a keresett szó alapján megjelenítendő ételekhez
+
+                $sql = "SELECT * FROM tbl_etel WHERE nev LIKE '%$search%' OR leiras LIKE '%$search%'";
+
+                $res = mysqli_query($conn, $sql);
+
+                $count = mysqli_num_rows($res);
