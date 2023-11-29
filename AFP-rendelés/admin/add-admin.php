f63a6614f5d422ec9baec0cdf3386aa21301962e
Attila <atti.denes@gmail.com>
2023-11-29 22:01:53 +0100

    post method és form, table tr1

--- AFP-rendelés/admin/add-admin.php
+++ AFP-rendelés/admin/add-admin.php
@@ -12,9 +12,23 @@
                 echo''.$_SESSION['add'].''; //üzenet kiírása
                 unset($_SESSION['add']); //üzenet eltávolítása
             }
+        ?>
+
+        <form action="" method="POST" >
+            
+            <table class="tbl-30">
+                <tr>
+                    <td>Teljes név: </td>
+                    <td>
+                        <input type="text" name="teljes_nev" placeholder="Írd be a teljes neved"  >
+                    </td>
+                </tr>
+
+            </table>
 
 
-         ?>
+
+        </form>
     </div>
 
 </div>
