f57bda849f3f616b594f003a10432128876295c6
jsztNoemi <ju.noni89@gmail.com>
2023-11-30 15:26:55 +0100

    menü section

---
+++ AFP-rendelés/foods.php
@@ -0,0 +1,16 @@
+<?php include('partials-front/menu.php');?>
+<!-- keresés -->
+<section class="food-search text-center">
+        <div class="container">
+            
+            <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
+                <input type="search" name="search" placeholder="Keresés" required>
+                <input type="submit" name="submit" value="Keresés" class="btn btn-primary">
+            </form>
+
+        </div>
+    </section>
+
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Ételek</h2>
