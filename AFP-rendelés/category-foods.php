0e148311db164fe0ba734df9fa2a059f90ab8ec8
ArchibaldLance <m.bendzsi10@gmail.com>
2023-11-30 23:17:42 +0100

    catfoods1

---
+++ AFP-rendelés/category-foods.php
@@ -0,0 +1,15 @@
+<?php include('partials-front/menu.php');?>
+
+<?php 
+
+    //id átlett e küldve vagy nem 
+    if(isset($_GET['category_id']))
+    {
+        // kategória id megvan 
+        $category_id = $_GET['category_id'];
+        // kategória neve az id alapján
+        $sql = "SELECT nev FROM tbl_kategoriak WHERE id=$category_id";
+
+        // sql futtatása
+
+        $res = mysqli_query($conn, $sql);
