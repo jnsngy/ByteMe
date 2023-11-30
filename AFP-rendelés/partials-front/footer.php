80d0cebc59547c269e341c68cd87720798246be5
jsztNoemi <ju.noni89@gmail.com>
2023-11-30 17:32:43 +0100

    főoldal footer elkészítése

---
+++ AFP-rendelés/partials-front/footer.php
@@ -0,0 +1,10 @@
+<!-- footer kezdete -->
+<section class="footer">
+        <div class="container text-center">
+        <p class="text-center">Bigga Nurger megbízásából készítette: Méhész Benjámin</p>
+        </div>
+    </section>
+    <!-- footer vége -->
+    
+</body>
+</html>
