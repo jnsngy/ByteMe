f74f65733ea21ad0a753bd664469e911154cd904
jnsngy <mail@jnsngy.hu>
2023-12-01 00:33:34 +0100

    pass1

--- AFP-rendelés/admin/update-password.php
+++ AFP-rendelés/admin/update-password.php
@@ -11,3 +11,32 @@
                 $id=$_GET['id'];
             }
         ?>
+         <form action="" method="POST">
+        <table class="tbl-30">
+                <tr>
+                    <td>Régi jelszó: </td>
+                    <td>
+                        <input type="password" name="jelenlegi_jelszo" placeholder="Jelenlegi jelszó">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Új jelszó: </td>
+                    <td>
+                        <input type="password" name="uj_jelszo" placeholder="Új jelszó">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Jelszó megerősítése: </td>
+                    <td>
+                        <input type="password" name="jelszo_megerosit" placeholder="Jelszó megerősítése">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                        <input type="submit" name="submit" value="Jelszó megváltoztatása" class="btn-secondary">
+                    </td>
+                </tr> 
