6931bd0944c2e1b23a2ab04b3fe4fbd67cebdbab
ArchibaldLance <m.bendzsi10@gmail.com>
2023-11-30 21:45:25 +0100

    login-file4

--- AFP-rendelés/admin/login.php
+++ AFP-rendelés/admin/login.php
@@ -28,3 +28,19 @@
         ?>
 
         <br><br>
+
+         <!-- bejelentkezés form -->
+         <form action="" method="POST" class="text-center">
+            Felhasználónév: <br>
+            <input type="text" name="username" placeholder="Felhasználónév megadása"><br><br>
+            Jelszó: <br>
+            <input type="password" name="password" placeholder="Jelszó megadása"><br><br>
+
+            <input type="submit" name="submit" value="Bejelentkezés" class="btn-primary"><br><br>
+
+        </form>
+
+
+    </div>
+</body>
+</html>
