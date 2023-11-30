811b882ca0a075e640c5a00d4d28d71d4823c655
ArchibaldLance <m.bendzsi10@gmail.com>
2023-11-30 21:45:13 +0100

    login-file3

--- AFP-rendelés/admin/login.php
+++ AFP-rendelés/admin/login.php
@@ -10,3 +10,21 @@
     <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
+
+<div class="login">
+        <h1 class="text-center">Bejelentkezés</h1> <br><br>
+
+        <?php
+            if(isset($_SESSION['login']))
+            {
+                echo $_SESSION['login']; //üzenet kiírása
+                unset($_SESSION['login']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['no-login-message']))
+            {
+                echo $_SESSION['no-login-message']; //üzenet kiírása
+                unset($_SESSION['no-login-message']); //üzenet eltávolítása
+            }
+        ?>
+
+        <br><br>
