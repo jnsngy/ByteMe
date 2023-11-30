9df97c3550197e4c612960f6b87bfcf4d6273353
jsztNoemi <ju.noni89@gmail.com>
2023-11-30 19:39:12 +0100

    van-e étel lekérdezés if ág

--- AFP-rendelés/order.php
+++ AFP-rendelés/order.php
@@ -1,1 +1,15 @@
 <?php include('partials-front/menu.php');?>
+
+<?php 
+
+    //van e étel id
+    if(isset($_GET['food_id']))
+    {
+        // étel id és adatok a kiválasztott ételről
+        $food_id = $_GET['food_id'];
+
+        $sql = "SELECT * FROM tbl_etel WHERE id=$food_id";
+
+        $res = mysqli_query($conn, $sql);
+
+        $count = mysqli_num_rows($res);
