c941f097db840a1d389d8f1a3518b4b0c3faff57
ArchibaldLance <m.bendzsi10@gmail.com>
2023-11-30 23:25:00 +0100

    up2

---
+++ AFP-rendelés/admin/update-category.php
@@ -0,0 +1,10 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Kategória szerkesztése</h1>
+
+        <br><br>
+
+        <?php
+            if(isset($_GET['id']))
