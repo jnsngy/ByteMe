3ec7023d45c88728bd9748097b8e511b40da204f
jsztNoemi <ju.noni89@gmail.com>
2023-11-30 18:36:31 +0100

    index.php include és sectionn

--- AFP-rendelés/index.php
+++ AFP-rendelés/index.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rendelés</title>
-</head>
-<body>
-    <h1>Ez itt a projekt, majd megkell változtatni ezt.</h1>
-</body>
-</html>
+<?php include('partials-front/menu.php');?>
+
+
+
+    <!-- keresés -->
+    <section class="food-search text-center">
+        <div class="container">
+            
+            <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
+                <input type="search" name="search" placeholder="Keresés" required>
+                <input type="submit" name="submit" value="Keresés" class="btn btn-primary">
+            </form>
+
+        </div>
+    </section>
+    <!-- keresés vége -->
+
+    
