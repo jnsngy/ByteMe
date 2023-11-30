75e61667d2b14ac5efc5faa6d6728103cd6d19ba
Attila <atti.denes@gmail.com>
2023-11-30 21:25:05 +0100

    if

--- AFP-rendelés/admin/update-admin.php
+++ AFP-rendelés/admin/update-admin.php
@@ -15,7 +15,22 @@
 
          $res = mysqli_query($conn, $sql);
 
-         
+         if($res==TRUE)
+         {
+             // sikeres, törlődött
+             // üzenet kiírására
+             $count = mysqli_num_rows($res);
+             // van e adat ellenrzése
+             if($count==1)
+             {
+                 //van adat
+                 $row=mysqli_fetch_assoc($res);
+
+                 $teljes_nev = $row['teljes_nev'];
+                 $felhasznalonev = $row['felhasznev'];
+
+             }
+        }
      
      
      
