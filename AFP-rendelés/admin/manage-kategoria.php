ec7e1f450acfe4d2128667fafabd02721d5fa4b3
jnsngy <mail@jnsngy.hu>
2023-12-01 00:24:48 +0100

    mankate2

---
+++ AFP-rendelés/admin/manage-kategoria.php
@@ -0,0 +1,21 @@
+<?php include('partials/menu.php');?>
+
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Kategória hozzáadása</h1>
+
+        <br /><br />
+
+        <?php
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add']; //üzenet kiírása
+                unset($_SESSION['add']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['remove']))
+            {
+                echo $_SESSION['remove']; //üzenet kiírása
+                unset($_SESSION['remove']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['delete']))
