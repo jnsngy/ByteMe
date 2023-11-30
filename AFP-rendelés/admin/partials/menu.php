b7d4191cca1fec96f76f5bf7ad90e22fc34a3cbf
jsztNoemi <ju.noni89@gmail.com>
2023-11-30 17:00:14 +0100

    html alap elkészítése

--- AFP-rendelés/admin/partials/menu.php
+++ AFP-rendelés/admin/partials/menu.php
@@ -4,3 +4,14 @@ include('../config/constants.php');
 include('login-check.php'); 
 
 ?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rendelés - Főoldal</title>
+    <link rel="stylesheet" href="../css/admin.css">
+
+</head>
+<body>
