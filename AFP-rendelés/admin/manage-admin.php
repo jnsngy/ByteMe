c807dc071acbc3398e0c2c50a2a929a31bd493a1
Attila <atti.denes@gmail.com>
2023-11-30 20:20:29 +0100

    main content

---
+++ AFP-rendelés/admin/manage-admin.php
@@ -0,0 +1,20 @@
+<?php include('partial/menu.php'); ?>
+
+//Fő téma
+
+<div class="main-content" >
+
+
+    <div class="wrapper" >
+
+        <h1>Admin irányító</h1>
+
+        <br /> <br />
+
+    </div>
+
+
+</div>
+
+
+<?php include('partials/footer.php'); ?>
