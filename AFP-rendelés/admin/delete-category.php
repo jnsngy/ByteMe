dd893ce6c7655caa07fe9432fecb2ca9111e4539
ArchibaldLance <m.bendzsi10@gmail.com>
2023-11-30 23:33:21 +0100

    delkat2

--- AFP-rendelés/admin/delete-category.php
+++ AFP-rendelés/admin/delete-category.php
@@ -8,3 +8,20 @@
         $kep_neve=$_GET['kep_neve'];
 
         //kép fájl törlése ha van
+
+        if($kep_neve != "")
+        {
+            $path = "../images/category/".$kep_neve;
+
+            $remove = unlink($path);
+            
+            //ha sikertelen a törlés akkor hibaüzenet és folyamat megszakítása
+            if($remove==FALSE)
+            {
+                $_SESSION['remove'] = "<div class='error'>Sikertelen törlés</div>";
+                header('location:'.SITEURL.'admin/manage-kategoria.php');
+
+                die();
+
+            }
+        }
