b79e561fa54233309e8917b06eb9964d440ff444
Attila <atti.denes@gmail.com>
2023-11-30 20:21:47 +0100

    if 1-3

--- AFP-rendelés/admin/manage-admin.php
+++ AFP-rendelés/admin/manage-admin.php
@@ -11,6 +11,25 @@
 
         <br /> <br />
 
+        <?php 
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add']; //üzenet kiírása
+                unset($_SESSION['add']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['delete']))
+            {
+                echo $_SESSION['delete']; //üzenet kiírása
+                unset($_SESSION['delete']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['update']))
+            {
+                echo $_SESSION['update']; //üzenet kiírása
+                unset($_SESSION['update']); //üzenet eltávolítása
+            }
+        
+        ?>
+
     </div>
 
 
