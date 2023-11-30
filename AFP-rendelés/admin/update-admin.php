e45a7344dc00288edd6b4b26eaaf30e9cad398b0
Attila <atti.denes@gmail.com>
2023-11-30 21:20:48 +0100

    div

---
+++ AFP-rendelés/admin/update-admin.php
@@ -0,0 +1,18 @@
+<?php include('partials/menu.php'); ?>
+<div class="main-content" >
+    <div class="wrapper" >
+        <h1>Admin szerkesztés</h1>
+        <br /> <br />
+
+        
+
+
+
+
+
+
+
+
+    </div>
+
+</div>
