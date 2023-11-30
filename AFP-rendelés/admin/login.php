4c53673344fa46cdc6946acf7f241f154769e6fb
ArchibaldLance <m.bendzsi10@gmail.com>
2023-11-30 21:45:00 +0100

    login-file2

---
+++ AFP-rendelés/admin/login.php
@@ -0,0 +1,12 @@
+<?php include('../config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bejelentkezés</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
