15d64e12cf26df14977cf7cedc37e3ed4a440566
jsztNoemi <ju.noni89@gmail.com>
2023-11-30 17:38:29 +0100

    include és html kezdő

---
+++ AFP-rendelés/partials-front/menu.php
@@ -0,0 +1,12 @@
+<?php  include('config/constants.php');?>
+
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bigga Nurger</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
