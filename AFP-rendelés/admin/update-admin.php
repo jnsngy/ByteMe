49e4d2de4f02f9d301b1a0825a3192ccd77d37c0
Attila <atti.denes@gmail.com>
2023-11-30 21:24:11 +0100

    sql query

--- AFP-rendelés/admin/update-admin.php
+++ AFP-rendelés/admin/update-admin.php
@@ -4,7 +4,22 @@
         <h1>Admin szerkesztés</h1>
         <br /> <br />
 
-        
+     <?php 
+         // törlendő admin ID
+         $id = $_GET['id'];
+
+         // sql a törlés végrehajtására
+         $sql = "SELECT * FROM tbl_admin WHERE id=$id";
+
+         // sql kód végrehajtása
+
+         $res = mysqli_query($conn, $sql);
+
+         
+     
+     
+     
+     ?>   
 
 
 
