9a29371a003f50e18999f1c69751b8b1ffd1ddbb
jnsngy <mail@jnsngy.hu>
2023-12-01 00:36:10 +0100

    kat3

--- AFP-rendelés/categories.php
+++ AFP-rendelés/categories.php
@@ -17,3 +17,22 @@
             <h2 class="text-center">Ételek felfedezése</h2>
 
             <?php 
+            //sql az adatok megjelenítéséhez
+            $sql = "SELECT * FROM tbl_kategoriak WHERE van_e='Igen'";
+            // kód futtatása
+            $res = mysqli_query($conn, $sql);
+            // sorok számolása hogy van e kategória
+            $count = mysqli_num_rows($res);
+
+            if($count > 0)
+            {
+                while($row = mysqli_fetch_assoc($res))
+                {
+                    $id = $row['id'];
+                    $title = $row['nev'];
+                    $image_name = $row['kep_neve'];
+                    ?>
+
+                    <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
+                        <div class="box-3 float-container">
+                            <?php 
