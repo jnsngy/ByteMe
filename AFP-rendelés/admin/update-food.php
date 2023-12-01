e0b31c0b522d2e0cb2ba55a22250db5085169061
Olajos Mate <mateolajos@gmail.com>
2023-12-01 11:38:52 +0100

    menü include + 1. if ág

---
+++ AFP-rendelés/admin/update-food.php
@@ -0,0 +1,17 @@
+<?php include('partials/menu.php'); ?>
+
+<?php 
+    if(isset($_POST['submit']))
+    {
+        $id = $_POST['id'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $price = $_POST['price'];
+        $current_image = $_POST['current_image'];
+        $category = $_POST['category'];
+
+        $featured = $_POST['featured'];
+        $active = $_POST['active'];
+     
+    }
+?>
