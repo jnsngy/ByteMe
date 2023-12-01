cb048bc299dd83a791089412955e50ad781d4b87
Olajos Mate <mateolajos@gmail.com>
2023-12-01 11:09:36 +0100

    if szerkezetek az add, delete, upload funkciókhoz

--- AFP-rendelés/admin/manage-etel.php
+++ AFP-rendelés/admin/manage-etel.php
@@ -7,7 +7,24 @@
 
         <br /><br />
 
-        
+        <?php
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add']; //üzenet kiírása
+                unset($_SESSION['add']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['delete']))
+            {
+                echo $_SESSION['delete']; //üzenet kiírása
+                unset($_SESSION['delete']); //üzenet eltávolítása
+            }
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload']; //üzenet kiírása
+                unset($_SESSION['upload']); //üzenet eltávolítása
+            }
+            
+        ?>
             
         </div>
     </div>
