701d926f3621dd3afde7527975d00cf1f5d742a3
Olajos Mate <mateolajos@gmail.com>
2023-12-01 11:50:53 +0100

    2-3 if ág

--- AFP-rendelés/admin/update-food.php
+++ AFP-rendelés/admin/update-food.php
@@ -12,6 +12,28 @@
 
         $featured = $_POST['featured'];
         $active = $_POST['active'];
+
+        if(isset($_FILES['image']['name']))
+        {
+            $image_name = $_FILES['image']['name'];
+
+            if($image_name!="")
+            {
+                $val = explode('.', $image_name);
+                $ext = end($val);
+
+                $image_name = "uj_kaja".rand(0000,9999).".".$ext;
+
+                $src_path = $_FILES['image']['tmp_name'];
+                $dest_path = "../images/food/".$image_name;
+
+                $upload = move_uploaded_file($src_path, $dest_path);
+            }
+            else
+            {
+                $image_name = $current_image;
+            }
+        }
      
     }
 ?>
