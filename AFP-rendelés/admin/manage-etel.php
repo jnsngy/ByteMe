a4e305abe4a0d200d6106607abd05741c48a1b8d
Olajos Mate <mateolajos@gmail.com>
2023-12-01 11:08:06 +0100

    menü include + alap div elemek + h1 címsor

---
+++ AFP-rendelés/admin/manage-etel.php
@@ -0,0 +1,15 @@
+<?php include('partials/menu.php');?>
+
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Étel irányító</h1>
+
+        <br /><br />
+
+        
+            
+        </div>
+    </div>
+
+<?php include('partials/footer.php'); ?> 
