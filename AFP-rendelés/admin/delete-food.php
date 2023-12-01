3bf4cad0572d62b04fc58ac4f2dfc263aff3038c
Olajos Mate <mateolajos@gmail.com>
2023-12-01 10:56:05 +0100

    include + id és név változók

---
+++ AFP-rendelés/admin/delete-food.php
@@ -0,0 +1,15 @@
+<?php
+
+    include('../config/constants.php');
+    //kep neve van-e vagy nem
+    if(isset($_GET['id']) AND isset($_GET['kep_neve']))
+    {
+        $id=$_GET['id'];
+        $kep_neve=$_GET['kep_neve'];
+
+        
+    }
+
+
+
+?>
